b368aae6c72352bf0d09c67ffe2e4966ad2c8813
여예원 <98465383+YewonYeo@users.noreply.github.com>
2022-08-29 02:43:26 +0900

    코딩애플 리액트 강의 part1

--- CodingApple/blog/src/App.js
+++ CodingApple/blog/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let [title, setTitle] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
+
   return (
+    // JSX: js파일에서 html 대신 쓰는 것
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+        <h4>Yeeeh's Blog</h4>
+      </div>
+      <div className="list">
+        <h4>{ title[0] }</h4>
+        <p>2022.08.29 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ title[1] }</h4>
+        <p>2022.08.29 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ title[2] }</h4>
+        <p>2022.08.29 발행</p>
+      </div>
     </div>
   );
 }
